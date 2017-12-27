49690551f242b0c8c9b3c5f6ee4b04dd105fa764
钟 文汉 <297334004@qq.com>
2017-12-27 15:03:36 +0800

    delete port
    1111

--- manage.py
+++ manage.py
@@ -2,6 +2,6 @@
 from app import app
 
 if __name__ == "__main__":
-	app.run(host="13.230.246.216",port=5001)
+	app.run(host="13.230.246.216")
 
 	# ssh -i /Users/zhongwenhan/Desktop/webservers/webServers.pem ubuntu@13.230.246.216
