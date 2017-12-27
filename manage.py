ec960d1810637a271df6e91d4d2b740d5f20aeb1
钟 文汉 <297334004@qq.com>
2017-12-27 14:57:12 +0800

    use the ip
    1111

--- manage.py
+++ manage.py
@@ -2,6 +2,6 @@
 from app import app
 
 if __name__ == "__main__":
-	app.run()
+	app.run(host="13.230.246.216",port=5001)
 
 	# ssh -i /Users/zhongwenhan/Desktop/webservers/webServers.pem ubuntu@13.230.246.216
